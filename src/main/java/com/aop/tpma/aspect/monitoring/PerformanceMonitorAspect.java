32b8bf63fb1526db7be1645b99b3845933d6e970
Gabriel <mihail.stanescu@metrosystems.net>
2020-05-06 19:03:02 +0300

    implemented memory usage aop tracking annotation

--- src/main/java/com/aop/tpma/aspect/monitoring/PerformanceMonitorAspect.java
+++ src/main/java/com/aop/tpma/aspect/monitoring/PerformanceMonitorAspect.java
@@ -1,9 +1,11 @@
 package com.aop.tpma.aspect.monitoring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
@@ -13,7 +15,7 @@ import org.springframework.util.StopWatch;
 @Component
 public class PerformanceMonitorAspect {
     @Around(value = "execution(* com.aop.tpma.service.*.*(..))")
-    public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public Object aroundTrackTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
 
         final StopWatch stopWatch = new StopWatch();
@@ -24,5 +26,12 @@ public class PerformanceMonitorAspect {
         log.info("Execution time of {}.{} :: {} ms", methodSignature.getDeclaringType().getSimpleName(), methodSignature.getName(), stopWatch.getTotalTimeMillis());
 
         return result;
+    }
+
+    @Before(value = "@annotation(com.aop.tpma.aspect.monitoring.annotation.MonitorMemoryUsageBefore)")
+    public void aroundTrackMemoryUsage(JoinPoint joinPoint) {
+        long memoryUsageBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        log.info("Memory usage: {} bytes before executing {}", memoryUsageBytes, joinPoint.getSignature());
+
     }
 }
